d3aa2d2dabc0f23697a33f190b415dea43570a57
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 17:03:14 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -10,4 +10,6 @@
 	$garbage = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key , $data , MCRYPT_MODE_CBC);
 	$content = base64_encode($garbage);
 	echo $content;
+	$plaintextprepare = base64_decode($content);
+	    $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext_prepare, MCRYPT_MODE_CBC);
 ?>
