808d2e832f5fdb6ec97749cf884dec2c13da2bdd
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 16:55:32 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -1,13 +1,12 @@
 
 <?php
-# This code sucks. 
+# This code sucks.
     $password = "password";
 	$hash = md5($password);
     $key = pack('H*', $hash);
     $data = "do-re-mi-fa-sol";
     $key_size =  strlen($key);
     echo "Key size: " . $key_size . "\n";
-    
 	$garbage = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key , $data , MCRYPT_MODE_CBC);
 	$content = base64_encode($garbage);
 	echo $content;
