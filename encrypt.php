78085170917df7bd22a0486ba95c26c87172a1fe
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 16:53:49 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -8,11 +8,13 @@
     $password = "password";
 	$hash = md5($password);
     $key = pack('H*', $hash);
-    
+    $data = "do-re-mi-fa-sol";
     # show key size use either 16, 24 or 32 byte keys for AES-128, 192
     # and 256 respectively
     $key_size =  strlen($key);
     echo "Key size: " . $key_size . "\n";
     
-
+	$garbage = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key , $data , MCRYPT_MODE_CBC);
+	$content = base64_encode($garbage);
+	echo $content;
 ?>
