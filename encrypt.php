0b4fd18da01a9597f6341b012825b2a6e84a06f0
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 16:55:05 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -1,16 +1,10 @@
 
 <?php
-    # --- ENCRYPTION ---
-
-    # the key should be random binary, use scrypt, bcrypt or PBKDF2 to
-    # convert a string into a key
-    # key is specified using hexadecimal
+# This code sucks. 
     $password = "password";
 	$hash = md5($password);
     $key = pack('H*', $hash);
     $data = "do-re-mi-fa-sol";
-    # show key size use either 16, 24 or 32 byte keys for AES-128, 192
-    # and 256 respectively
     $key_size =  strlen($key);
     echo "Key size: " . $key_size . "\n";
     
