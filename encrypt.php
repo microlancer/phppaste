126b52b7c2c7bd8811b9ee46244c73b2e4bcb6f2
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 17:06:44 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -6,11 +6,11 @@
     $key = pack('H*', $hash);
     $data = "do-re-mi-fa-sol";
     $key_size =  strlen($key);
-    echo "Key size: " . $key_size . "\r\n";
+    echo "Key size: " . $key_size . PHP_EOL;
 	$garbage = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key , $data , MCRYPT_MODE_CBC);
 	$content = base64_encode($garbage);
-	echo $content . "\r\n";
+	echo $content . PHP_EOL;
 	$plaintextprepare = base64_decode($content);
 	$plaintext_decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext_prepare, MCRYPT_MODE_CBC);
-	echo $plaintext_decrypted . "\r\n";
+	echo $plaintext_decrypted . PHP_EOL ;
 ?>
