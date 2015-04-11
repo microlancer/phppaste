43b67c16c63ac1cdeb61c2da24b6beea4a31272d
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 20:46:37 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -45,11 +45,11 @@
 # This code sucks.
     $password = "password";
 	$hash = $password;
-    $key = pack('H*', $hash);
+	$key = $hash
+#    $key = pack('H*', $hash);
     $data = $_POST['input'];
     $key_size =  strlen($key);
     echo "<pre>";
-    echo $key;
     echo "Key size: " . $key_size . PHP_EOL;
 	$garbage = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key , $data , MCRYPT_MODE_CBC);
 	$content = base64_encode($garbage);
