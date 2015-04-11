3fe6e2b0588a99ca2e331eba45be44c15369b3d4
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-11 16:47:49 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -14,45 +14,5 @@
     $key_size =  strlen($key);
     echo "Key size: " . $key_size . "\n";
     
-    $plaintext = "This string was AES-256 / CBC / ZeroBytePadding encrypted.";
 
-    # create a random IV to use with CBC encoding
-    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-    
-    # creates a cipher text compatible with AES (Rijndael block size = 128)
-    # to keep the text confidential 
-    # only suitable for encoded input that never ends with value 00h
-    # (because of default zero padding)
-    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key,
-                                 $plaintext, MCRYPT_MODE_CBC, $iv);
-
-    # prepend the IV for it to be available for decryption
-    $ciphertext = $iv . $ciphertext;
-    
-    # encode the resulting cipher text so it can be represented by a string
-    $ciphertext_base64 = base64_encode($ciphertext);
-
-    echo  $ciphertext_base64 . "\n";
-
-    # === WARNING ===
-
-    # Resulting cipher text has no integrity or authenticity added
-    # and is not protected against padding oracle attacks.
-    
-    # --- DECRYPTION ---
-    
-    $ciphertext_dec = base64_decode($ciphertext_base64);
-    
-    # retrieves the IV, iv_size should be created using mcrypt_get_iv_size()
-    $iv_dec = substr($ciphertext_dec, 0, $iv_size);
-    
-    # retrieves the cipher text (everything except the $iv_size in the front)
-    $ciphertext_dec = substr($ciphertext_dec, $iv_size);
-
-    # may remove 00h valued characters from end of plain text
-    $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,
-                                    $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-    
-    echo  $plaintext_dec . "\n";
 ?>
