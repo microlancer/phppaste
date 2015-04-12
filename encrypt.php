7ad02dcbab54321e46512d4bb1517243b5354942
thejustaguy <justaguy AT justaguy DOT pw>
2015-04-12 23:46:10 +0200

    -

--- encrypt.php
+++ encrypt.php
@@ -57,18 +57,6 @@
 	$plaintext_decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $plaintextprepare, MCRYPT_MODE_CBC);
 	echo $plaintext_decrypted . PHP_EOL ;
 	echo "</pre>";
-    $diff = $max - $min;
-    if ($diff < 0 || $diff > 0x7FFFFFFF) {
-	throw new RuntimeException("Bad range");
-    }
-    $bytes = mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
-    if ($bytes === false || strlen($bytes) != 4) {
-        throw new RuntimeException("Unable to get 4 bytes");
-    }
-    $ary = unpack("Nint", $bytes);
-    $val = $ary['int'] & 0x7FFFFFFF;   // 32-bit safe
-    $fp = (float) $val / 2147483647.0; // convert to [0,1]
-    echo round($fp * $diff) + $min;
-
+	echo mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
 			}
 ?>
